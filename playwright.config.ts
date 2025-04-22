f4c2365dfe5385029bca43e070b2793d144076f3
avena <esra@retrace.ai>
2025-04-22 13:02:38 +0300

    updated the webserver options

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,7 +73,8 @@ export default defineConfig<TestOptions>({
   ],
   webServer :{
     command: 'npm run start',
-    url:'http://localhost:4200/'
+    url:'http://localhost:4200/',
+    reuseExistingServer: true,
   }
     
    
